6b1860ea3c6effbb89cf34598dba679cadf6b37d
Arooj Chaudhry <s204759@student.dtu.dk>
2023-01-12 07:02:55 +0100

    Button for entering the username

--- src/main/java/group2/GUI/Lobby.java
+++ src/main/java/group2/GUI/Lobby.java
@@ -3,10 +3,13 @@ package group2.GUI;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Lobby {
 	
@@ -14,9 +17,13 @@ public class Lobby {
 	Container con;
 	JLabel textLabel;
 	JPanel textPanel;
+	JPanel inputPanel;
+	JTextField tf;
+	JButton button;
 
 	
 	Font  f1  = new Font(Font.SERIF, Font.PLAIN,  23);
+	
 	
 	public static void main(String[] args) {
 		new Lobby();
@@ -39,7 +46,7 @@ public class Lobby {
 		
 		textPanel = new JPanel();
 		textPanel.setBounds(140,240,500,100);
-		textPanel.setBackground(Color.blue);
+		textPanel.setBackground(Color.white);
 		con.add(textPanel);
 		
 		
@@ -49,9 +56,29 @@ public class Lobby {
 		textLabel.setForeground(Color.black);
 		textLabel.setFont(f1);
 		textPanel.add(textLabel);
+		
+		
+		//Able to enter the username
+		
+		inputPanel = new JPanel();
+		inputPanel.setBounds(150,420,500,50);
+		inputPanel.setBackground(Color.black);
+		inputPanel.setLayout(new GridLayout(1,2));
+		
+		tf = new JTextField();
+		inputPanel.add(tf);
+		
+		//button for entering username
+		button = new JButton("Enter");
+		button.setForeground(Color.black);
+		inputPanel.add(button);
+		con.add(inputPanel);
+		
 		screen.setVisible(true);
 		
-		//TODO: Able to enter the username
+		//TODO: create an input handler to go to next phase after entering the name
+		
+		
 		
 		
 	}
